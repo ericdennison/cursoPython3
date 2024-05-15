5aefb0db1ebf4c6b24c915223643a177b1d3b20e
eddesks <ericdenniso@gmail.com>
2024-05-15 09:01:51 -0300

    first commit

---
+++ mod2instconfig/a4Ubuntu22.py
@@ -0,0 +1,27 @@
+# COMANDOS TERMINAL 
+
+# sudo apt update -y
+# sudo apt upgrade -y
+# sudo apt install git curl build-essential -y
+# sudo apt install gcc make default-libmysqlclient-dev libssl-dev -y
+# sudo apt install python3.10-full python3.10-dev -y
+
+# echo $SHELL
+
+# python3 --version
+# python3 -V
+# python3 -m venv venv
+
+# source venv/bin/activate
+# wich python
+
+# pip install pymysql
+# python -m pip install --upgrade
+
+# deactivate
+
+# comando para abrir pasta diretamente no vscode -> code .
+
+# rm -R apagarpasta
+
+print(1 + 1)
