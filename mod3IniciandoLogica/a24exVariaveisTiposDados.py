fce1caaadde796c97235a03fc461002affcd38b4
eddesks <ericdenniso@gmail.com>
2024-05-16 16:57:49 -0300

    criado - a24exVarivaeisTiposDados.py

---
+++ mod3IniciandoLogica/a24exVariaveisTiposDados.py
@@ -0,0 +1,15 @@
+
+nome = 'Eric'
+sobrenome = 'Dennison'
+idade = 35
+ano_nascimento = 1989
+maior_de_idade = idade >= 18
+altura_metros = 1.69
+
+
+print('Nome:', nome)
+print('Nome:', sobrenome)
+print('Nome:', idade)
+print('Nome:', ano_nascimento)
+print('Nome:', maior_de_idade)
+print('Nome:', altura_metros)
