e7524849c8df2964c7c53e6238ba9f32fc0b30a1
eddesks <ericdenniso@gmail.com>
2024-05-16 16:29:50 -0300

    criado - a22conversaoTipos.py

---
+++ mod3IniciandoLogica/a22conversaoTipos.py
@@ -0,0 +1,16 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#  tipos imutáveis e primitivos:
+# str, int, float, bool
+
+#print('1' + 1)
+#print('a' + 'b')
+
+#conversão
+print(int('1'), type('1'))
+print(int('1')+1)
+print(float('1')+1)
+print(type(float('1')+1))
+print(bool(' '))
+print(str(11) + 'b')
