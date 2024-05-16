a6a67fd4c6fb74b5cd426464ff4ccf1953e7d830
eddesks <ericdenniso@gmail.com>
2024-05-16 14:34:46 -0300

    criado a19tipoString.py

---
+++ mod3IniciandoLogica/a19tipoString.py
@@ -0,0 +1,25 @@
+"""
+
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+
+"""
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print('Luiz "Otávio"')
+
+
+# Aspas duplas
+print("Luiz Otávio")
+print("Luiz 'Otávio'")
+
+#Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
+
