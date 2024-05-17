455166ab44a4842b330853e56f14fa98585cb2ec
eddesks <ericdenniso@gmail.com>
2024-05-17 16:32:16 -0300

    criado a30resIMC

---
+++ mod3IniciandoLogica/a30resIMC.py
@@ -0,0 +1,15 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+print(nome,'tem ',altura,'de altura,',)
+print('pesa',peso,'quilos e seu IMC é,',)
+print(imc)
+
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é 
+# 29.320984654320987
+# IMC = peso / (altura x altura)
+
