0eb180a462949eaa083a0a3d2ec63ea70df86ee9
eddesks <ericdenniso@gmail.com>
2024-05-17 16:25:43 -0300

    criado a29exIMC

---
+++ mod3IniciandoLogica/a29exIMC.py
@@ -0,0 +1,19 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+# print(nome,'tem ',altura,',\n pesa ',peso,'quilos e seu IMC é \n',imc)
+
+print('Nome: ', nome)
+print('Altura: ', altura)
+print('Peso: ', peso)
+print('IMC: ', imc)
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é 
+# 29.320984654320987
+# IMC = peso / (altura x altura)
+
+# Ellipsis
+print(...)
