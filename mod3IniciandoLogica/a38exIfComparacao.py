07cac3bd4314c24edbcbb10adf0a95e71dd19d2e
eddesks <ericdenniso@gmail.com>
2024-05-21 16:52:17 -0300

    a38exIfComparacao.py

---
+++ mod3IniciandoLogica/a38exIfComparacao.py
@@ -0,0 +1,5 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'primeiro={primeiro_valor} é maior que o {segundo_valor}')
