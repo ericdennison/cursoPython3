0ddc3a171725ab62a16dccdf99e1940b72ce276e
eddesks <ericdenniso@gmail.com>
2024-05-21 16:38:10 -0300

    criado arquivo a37operadoresRelacionais

---
+++ mod3IniciandoLogica/a37operadoresRelacionais.py
@@ -0,0 +1,25 @@
+# Comparação
+
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2 
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print(maior_ou_igual)
+
+
+# Shell Interativo Python
+# python3 -i modulo.py
