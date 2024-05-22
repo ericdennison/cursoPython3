2b929595d043856b73bb32058385743f01ffcef5
eddesks <ericdenniso@gmail.com>
2024-05-22 15:13:07 -0300

    modificado a38exIfComparacao, criado a39resIfComparacao

--- mod3IniciandoLogica/a38exIfComparacao.py
+++ mod3IniciandoLogica/a38exIfComparacao.py
@@ -2,11 +2,11 @@ primeiro_valor = input('Digite um valor: ')
 segundo_valor = input('Digite outro valor: ')
 
 if primeiro_valor > segundo_valor:
-    print(f'primeiro="{primeiro_valor}" é maior que o segundo"{segundo_valor}"')
+    print(f'primeiro_valor="{primeiro_valor}" é maior que o segundo_valor="{segundo_valor}"')
 elif primeiro_valor < segundo_valor:
-    print('Segundo valor é maior')
+    print(f'segundo_valor="{segundo_valor}" é maior que o primeiro_valor="{primeiro_valor}"')
 else:
-    print('Tente novamente')
+    print('Valores Iguais')
 
 
 
@@ -15,8 +15,8 @@ else:
 
 # Beecrowd
 # Python 3.11
-raio = float(input())
-pi = 3.14159
-area = pi*(raio**2)
-print('{:.2f}'.format(area))
-print(f'A={area:.4f}')
+# raio = float(input())
+# pi = 3.14159
+# area = pi*(raio**2)
+# print('{:.2f}'.format(area))
+# print(f'A={area:.4f}')
