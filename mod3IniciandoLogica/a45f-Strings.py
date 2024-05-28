d9e8969d64fccf02a1e632b45f147ea0a3a9a73e
eddesks <ericdenniso@gmail.com>
2024-05-28 16:21:25 -0300

    modificado a45f-String

--- mod3IniciandoLogica/a45f-Strings.py
+++ mod3IniciandoLogica/a45f-Strings.py
@@ -5,7 +5,7 @@ d - int
 f - float
 .<número de dígitos>f
 x ou X - Hexadecimal
-(Caractere)(><^)(quantidade)
+(Caractere)(><^)(quantidade/preechimento)
 > - Esquerda
 < - Direita
 ^ - Centro
@@ -14,3 +14,11 @@ Sinal - + ou -
 Ex.: 0>-100,.1f
 Conversion flags - !r !s !a 
 """
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.4873648123746:0=+10,.1f}')
+print('o hexadecimal de 1500 é {1500:08x}')
