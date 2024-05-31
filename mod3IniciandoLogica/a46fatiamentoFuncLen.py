1eaff842f0ce543b6a8054f25f87ff5c23d98e8e
eddesks <ericdenniso@gmail.com>
2024-05-31 14:47:49 -0300

    criado a46fatiamentoFuncLen.py

---
+++ mod3IniciandoLogica/a46fatiamentoFuncLen.py
@@ -0,0 +1,22 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+
+variavel = 'Olá mundo'
+print(variavel[4:])
+print(variavel[4:8])
+print(variavel[0:5])
+print(variavel[:5])
+print(variavel[-8:-2])
+print(len(variavel[3]))
+print(variavel[0:len(variavel):1])
+print(variavel[0:9:1])#pegando do inicio 0 e finalizando no 9 de 1 em um passo
+print(variavel[0:9:2])
+print(variavel[::-1])
+print(variavel[-1:-10:-1])
