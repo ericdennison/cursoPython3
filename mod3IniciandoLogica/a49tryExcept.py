cb836b7ea98c096ea6f0a1141475f17b052ad8fe
eddesks <ericdenniso@gmail.com>
2024-06-03 16:57:48 -0300

    criado a49tryExcept

---
+++ mod3IniciandoLogica/a49tryExcept.py
@@ -0,0 +1,35 @@
+# Exceptions
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+
+# print(1234)
+# print(456)
+# int('a')
+
+# numero = input('Vou dobrar o número que vc digitar: ')
+
+# numero_float = float(numero)
+# print(f'O dobro de {numero} é {numero_float * 2:.2f}')
+
+
+numero_str = input(
+    'Vou dobrar o número que vc digitar'
+
+)
+
+try:
+     print('STR: ', numero_str)
+     numero_float = float(numero_str)
+     print('FLOAT: ', numero_float)
+     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
+
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso não é um número')
