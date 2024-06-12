61668bbf5d3a45965c5036f72ef12fc77c876bfc
eddesks <ericdenniso@gmail.com>
2024-06-12 15:41:04 -0300

    a61atribuicaoAritmeticos

---
+++ mod3IniciandoLogica/a61atribuicaoAritmeticos.py
@@ -0,0 +1,17 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 1
+contador1 = '1'
+contador2 = 10
+
+###
+
+contador += 2
+contador1 += '2'
+contador2 *= '2'
+print(contador)
+print(contador1)
+print(contador2)
