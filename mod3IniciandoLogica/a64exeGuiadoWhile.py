04b8e1f1c143ac058fd964b42fcac2bf0cecbc6f
eddesks <ericdenniso@gmail.com>
2024-06-13 15:58:08 -0300

    a64exeGuiadoWhile

---
+++ mod3IniciandoLogica/a64exeGuiadoWhile.py
@@ -0,0 +1,12 @@
+"""
+Iterando strings com while
+"""
+#       012345678910
+nome = 'Luiz Otávio' #Iteráveis
+#      1110987654321
+
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+
+print(nome[3])
