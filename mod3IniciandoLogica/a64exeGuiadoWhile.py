ecc225b3d7825cc9477a23fea687a3341d74b5d7
eddesks <ericdenniso@gmail.com>
2024-06-17 14:44:38 -0300

    criado a64exeGuiadoWhile

--- mod3IniciandoLogica/a64exeGuiadoWhile.py
+++ mod3IniciandoLogica/a64exeGuiadoWhile.py
@@ -10,3 +10,9 @@ print(nome)
 print(tamanho_nome)
 
 print(nome[3])
+contador = 0
+novo_nome = ''
+while contador < len(nome):
+    novo_nome += "*"+nome[contador] # novo_nome = novo_nome('') + "*" + nome[contador] 0
+    print(novo_nome)
+    contador += 1 
