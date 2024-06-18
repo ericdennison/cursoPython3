41ab7e6a67e0298242e6f12ac8c52ab6a663e8fb
eddesks <ericdenniso@gmail.com>
2024-06-18 15:25:59 -0300

    criado a70iterandoStringWhile

---
+++ mod3IniciandoLogica/a70iterandoStringWhile.py
@@ -0,0 +1,45 @@
+'''
+barra invertida \ para quebra de linha
+count -> contagem strings iguais
+'''
+frase = 'O Python é uma linguagem de programção'\
+        'multiparadigam.'\
+        'Python foi criado por Guido Van Rossum'
+
+print(frase.count('Python')) #2
+
+# i = 0
+# contagem = 0
+# while i < len(frase):
+#     letra_atual = frase[i]
+
+#     print(letra_atual)
+#     i += 1
+#     contagem += 1
+# print(contagem)
+
+# resposta Professor
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_vezes_letra_apareceu_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_vezes_letra_apareceu_atual:
+        qtd_apareceu_mais_vezes = qtd_vezes_letra_apareceu_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    
+   
+print('A letra que apareceu mais vezes foi '
+      f'"{letra_apareceu_mais_vezes}" que apareceu '
+      f'{qtd_apareceu_mais_vezes}x')
+    
