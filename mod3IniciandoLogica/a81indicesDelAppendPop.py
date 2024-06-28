771c2bac860b2591a9aa51feb0903078d5b76dff
ericdennison <ericdenniso@gmail.com>
2024-06-28 15:39:13 -0300

    a81indicesDelAppendPop

---
+++ mod3IniciandoLogica/a81indicesDelAppendPop.py
@@ -0,0 +1,22 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+lista[2] = 300 # adiciona
+del lista[2] # deleta
+print(lista)
+print(lista[2])
+lista.append(50) # adiciona ultimo valor
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop() # deleta ultimo valor
+lista.pop(3) #deleta por indice
+print(lista, 'Removido: ',ultimo_valor)
