10c30df7e742e8fd638bbb0e3fb2700b558e283b
ericdennison <ericdenniso@gmail.com>
2024-07-04 16:01:25 -0300

    04072024 - new file:   a88tuplas.py

---
+++ mod3IniciandoLogica/a88tuplas.py
@@ -0,0 +1,16 @@
+"""
+Tipo tupla - Uma lista imutável
+
+formas de se criar uma tupla
+
+nomes = 'Maria', 'Helena', 'Luiz'
+nomes = ('Maria', 'Helena', 'Luiz')
+"""
+
+# converter lista em tupla e tuplas em lista
+nomes = ['Maria', 'Helena', 'Luiz']
+nomes = tuple(nomes)
+nomes = list(nomes)
+print(nomes[-1])
+print(nomes)
+
