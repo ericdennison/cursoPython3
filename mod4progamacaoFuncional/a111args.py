22ab8e9bf2c9a0c1f0421d65775471b0533a7631
eddesks <ericdenniso@gmail.com>
2024-07-29 13:55:49 -0300

    criado a111 e a112

--- mod4progamacaoFuncional/a111args.py
+++ mod4progamacaoFuncional/a111args.py
@@ -2,3 +2,21 @@
 args - Argumentos não nomeados
 * - *args (empacotamento e desempacotamento)
 """
+
+# Lembrete de desempacotamento
+x,y,*resto = 1, 2, 3, 4
+print(x, y, resto)
+
+def soma(*args):
+    #acumulador
+    total = 0
+    for numero in args:
+        print('Total', total, numero)
+        total = total + numero
+        print('Total', total)
+    print(total)
+
+
+soma(1, 2, 3, 4, 5, 6)
+
+
