b38c9c2081387afec780ecad86a70811184673af
eddesks <ericdenniso@gmail.com>
2024-08-23 15:29:23 -0300

    criado a127, 128, 129, 130

--- mod4progamacaoFuncional/a127_128_129mutavelSetPython.py
+++ mod4progamacaoFuncional/a127_128_129mutavelSetPython.py
@@ -19,15 +19,6 @@
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
 
-# Métodos úteis:
-# add, update, clear, discard
-
-# Operadores úteis:
-# união | união (union) - Une
-# intersecção & (intersection) - Itens presentes em ambos
-# diferença - Itens presentes apenas no set da esquerda
-# diferença simétrica ^ - Itens que não estão em ambos
-
 # s1 = set('Luiz')
 # ou
 # s1 = {'Luiz', 1, 2, 3}
@@ -36,3 +27,44 @@
 # elimina valores repetidos
 # s1 = {1,2,3,3,3,3,3,3,1}
 # print(s1)
+
+# l1 = [1,2,3,3,3,3,3,1]
+# s1 = set(l1)
+# l2 = list(s1)
+# s1 = {1, 2, 3}
+# print(4 in s1)
+# print(3 not in s1)
+
+# for numero in s1:
+#     print(numero)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# s1 = set()
+# s1.add('Luiz')
+# s1.add(1)
+# s1.update(('olá mundo',1,2,3,4))
+# # s1.clear()
+# s1.discard('olá mundo')
+# s1.discard('Luiz')
+# print(s1)
+
+
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s4 = s1 & s2
+s5 = s1 - s2
+s6 = s1 ^ s2
+print(s3)
+print(s4)
+print(s5)
+print(s6)
