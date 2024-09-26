83f7ac35364991c1bca8067e00123a3dc0c66f20
eddesks <ericdenniso@gmail.com>
2024-09-26 16:02:47 -0300

    criado a139 e a140

---
+++ mod4progamacaoFuncional/a140maisDetalhes.py
@@ -0,0 +1,5 @@
+'''
+Mais detalhes sobre list comprehension
+Eu criei um vídeo gratuito falando muito mais sobre list comprehension tirando como base as dúvidas desse curso. Veja em https://youtu.be/1YbTDczvqco
+
+'''
