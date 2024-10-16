a34114a8e222a4a1b466657e5304c41c473f4384
eddesks <ericdenniso@gmail.com>
2024-10-16 16:29:49 -0300

    a146

--- mod4progamacaoFuncional/a146generatorExpressionIterablesIterators .py
+++ mod4progamacaoFuncional/a146generatorExpressionIterablesIterators .py
@@ -1,5 +1,14 @@
+import sys
 # Generator expression, Iterables e Iterators em Python
 
 
 iterable = ['Eu', 'Tenho', '_iter_']
-iterator = iter(iterable)#tem _iter_ e _next_
+iterator = iter(iterable) # tem _iter_ e _next_
+lista = [n for n in range(1000000)]
+generator = (n for n in range(1000000))
+
+# tamanho
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
+
+print(len(generator))
