3b6f86423fd759b2492d8b349a881219a95015b3
eddesks <ericdenniso@gmail.com>
2024-10-18 14:58:49 -0300

    a147

--- mod4progamacaoFuncional/a147introducaoGenerator.py
+++ mod4progamacaoFuncional/a147introducaoGenerator.py
@@ -1,18 +1,28 @@
 # Introdução às Generator functions em Python
 # generator = (n for n in range(1000000))
 
-def generator(n=0):
-    yield 1
+def generator(n=0, maximum=10):
+    # yield 1
+    # # return 'ACABOU'
+    # print('Continuando...')
+    # yield 2
+    # print('Mais uma')
+    # yield 3
+    # print('Vou terminar')
     # return 'ACABOU'
-    print('Continuando...')
-    yield 2
-    print('Mais uma')
-    yield 3
-    print('Vou terminar')
-    return 'ACABOU'
-    
+    while True:
+        yield n
+        n += 1
 
-gen = generator(n=0)
-print(next(gen))
-print(next(gen))
-print(next(gen))
+
+        if n >= maximum:
+            return 
+        
+
+gen = generator()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# ou usando for
+for n in gen:
+    print(n)
