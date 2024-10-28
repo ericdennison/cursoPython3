fd99a4032bc75043b9e165b6b94bfba8ab1af16a
eddesks <ericdenniso@gmail.com>
2024-10-28 16:50:15 -0300

    a149

---
+++ mod4progamacaoFuncional/a149tryExceptp1.py
@@ -0,0 +1,24 @@
+# (Parte1) try e except para tratar exceções
+# Try, except, else e finally
+# a = 18
+# b = 0
+# c = a / b
+
+
+try:
+    a = 18
+    b = 0
+    print(b[0])
+    print("Linha 1")
+    c = a / b
+    print("Linha 2")
+except ZeroDivisionError:
+    print("Dividiu por zero.")
+except NameError:
+    print("Nome b não está definido")
+except (TypeError, IndexError):
+    print("TypeError")
+except Exception:
+    print("ERRO DESCONHECIDO.")
+
+print("CONTINUAR")
