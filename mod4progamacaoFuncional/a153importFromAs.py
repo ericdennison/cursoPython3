29eb720fbc87013754488a2fe702c62e4ba491e9
eddesks <ericdenniso@gmail.com>
2024-11-08 15:34:06 -0300

    criado a153, a154

--- mod4progamacaoFuncional/a153importFromAs.py
+++ mod4progamacaoFuncional/a153importFromAs.py
@@ -3,21 +3,43 @@
 # inteiro - import nome_modulo
 # Vantagens: você tem o namespace do módulo
 # Desvantagens: nomes grandes
-import sys
+# import sys
 
-# sys.exit()
-# print(123)
-print(sys.platform)
+# # sys.exit()
+# # print(123)
+# platform = 'A MINHA'
+# print(sys.platform)
+# print(platform)
 
 # partes - from nome_modulo import objeto1, objeto2
 # Vantagens: nomes pequenos
 # Desvantagens: Sem o namespace do módulo
+# from sys import exit, platform
+
+# print(platform)
+
 
 # alias 1 - import nome_modulo as apelido
+# import sys as s
+
+# sys = 'alguma coisa'
+# print(s.platform)
+# print(sys)
+
 # alias 2 - from nome_modulo import objeto as apelido
+# from sys import exit as ex
+# from sys import platform as pf
+
+# print(pf)
+
+
 # Vantagens: você pode reservar nomes para seu código
 # Desvantagens: pode ficar fora do padrão da linguagem
 
 # má prática - from nome_modulo import *
 # Vantagens: importa tudo de um módulo
 # Desvantagens: importa tudo de um módulo
+# from sys import *
+
+# print(platform)
+# exit()
