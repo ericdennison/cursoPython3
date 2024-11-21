6368026e093568f84ffd32b87419edd3cd1b51dc
eddesks <ericdenniso@gmail.com>
2024-11-21 10:38:29 -0300

    criado a157

---
+++ mod4progamacaoFuncional/a157package/modulo.py
@@ -0,0 +1,16 @@
+# importando apenas o que quero através de (__all__ *)
+__all__ = [
+    'variavel', 
+    'multiplicador_do_modulo',
+    'nova_variavel'
+]
+
+variavel = "Alguma coisa"
+
+def soma_do_modulo(x, y):
+    return x + y
+
+def multiplicador_do_modulo(x, y):
+    return x * y
+
+nova_variavel = 'Ok'
---
+++ mod4progamacaoFuncional/a157packages.py
@@ -0,0 +1,18 @@
+from sys import path
+# import a157package
+import a157package.modulo
+from a157package.modulo import soma_do_modulo
+from a157package import modulo
+from a157package.modulo import *
+
+print(__name__)
+
+print(*path, sep='\n')
+
+print(soma_do_modulo(1,2))
+print(a157package.modulo.soma_do_modulo(3,3))
+print(modulo.soma_do_modulo(6,3))
+print(variavel)
+print(multiplicador_do_modulo(2,2))
+print(nova_variavel)
+
