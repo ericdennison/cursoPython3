03d2f6690983fbaf93bdec64512f76adf331b2dc
eddesks <ericdenniso@gmail.com>
2024-12-04 16:31:05 -0300

    alterado a160

--- mod4progamacaoFuncional/a160exe3Exercicios.py
+++ mod4progamacaoFuncional/a160exe3Exercicios.py
@@ -1,7 +1,13 @@
+import copy
+
+
 # copy, sorted, produtos.sort
 # Exercícios
 # Aumente os preços dos produtos a seguir em 10%
 # Gere novos_produtos por deep copy (cópia profunda)
+
+
+
 produtos = [
     {'nome': 'Produto 5', 'preco': 10.00},
     {'nome': 'Produto 1', 'preco': 22.32},
@@ -15,3 +21,32 @@ produtos = [
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+produtos = [
+
+    {**produto,'preco':round(produto['preco'] + ((produto['preco']/100)*10), 2)}
+    for produto in produtos
+]
+
+novos_produtos = copy.deepcopy(produtos)
+
+novos_produtos.append({'nome': 'Produto 6', 'preco': 50.00})
+novos_produtos.append({'nome': 'Produto 7', 'preco': 30.33})
+novos_produtos.append({'nome': 'Produto 8', 'preco': 40.00})
+
+print(novos_produtos)
+print()
+
+
+
+copia_novos_produtos = copy.deepcopy(novos_produtos)
+
+
+produtos_ordenados_por_nome = sorted(copia_novos_produtos, key=lambda item: item['nome'])
+produtos_ordenados_por_preco = sorted(copia_novos_produtos, key=lambda item: item['preco'])
+
+
+print(produtos_ordenados_por_nome, sep='\n')
+print()
+print(produtos_ordenados_por_preco, sep='\n')
+
