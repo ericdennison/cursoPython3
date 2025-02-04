426a9048fa93a9d1e214b4232d3a762f4bd227e3
eddesks <ericdenniso@gmail.com>
2025-02-04 11:22:43 -0300

    04/02/25 11:22 criado a162

--- mod4progamacaoFuncional/a162exeAdiandoExecucaoFuncao.py
+++ mod4progamacaoFuncional/a162exeAdiandoExecucaoFuncao.py
@@ -7,9 +7,15 @@ def multiplica(x, y):
     return x * y
 
 
-def criar_funcao(funcao, *args):
-    return funcao(*args)
-
+def criar_funcao(funcao, x):
+    def interna(y):
+        # pausado esperando closure
+        return funcao(x, y)
+    return interna
 
 soma_com_cinco = criar_funcao(soma, 5)
 multiplica_por_dez = criar_funcao(multiplica, 10)
+
+# feito closure(fechamento)
+print(soma_com_cinco(10))
+print(multiplica_por_dez(10))
