7465119bb3f7a45563c61e04d3f2ee55e7c5933e
eddesks <ericdenniso@gmail.com>
2025-08-13 15:50:23 -0300

    13082025 - atualização a19

--- mod3IniciandoLogica/a19tipoString.py
+++ mod3IniciandoLogica/a19tipoString.py
@@ -23,3 +23,39 @@ print("Luiz \"Otávio\"")
 # r
 print(r"Luiz \"Otávio\"")
 
+'''
+o r antes da string significa raw string (“string crua”).
+
+Isso faz com que o Python não interprete as barras invertidas (\) como caracteres de escape.
+Ou seja, dentro de uma raw string, \n não vira quebra de linha, \t não vira tabulação, e \" é mantido exatamente como \".
+
+
+Tipagem Dinâmica — o tipo é decidido em tempo de execução
+* Em Python, você não declara o tipo da variável (int, str, float, etc.) no código.
+
+* O tipo é inferido automaticamente no momento em que você atribui um valor.
+
+* O mesmo nome de variável pode apontar para objetos de tipos diferentes ao longo do programa.
+
+Exemplo: 
+x = 10        # x é int agora
+x = "texto"   # agora x é str
+
+Tipagem Forte — o Python não faz conversões automáticas inseguras
+* Uma linguagem de tipagem fraca permitiria misturar tipos de forma implícita (ex: somar número com texto sem erro).
+
+* Em Python, se você tentar operar tipos incompatíveis sem conversão explícita, vai dar erro.
+
+Exemplo:
+x = "10"
+y = 5
+print(x + y)  # ❌ TypeError
+É preciso converter manualmente:
+print(int(x) + y)  # ✅ 15
+
+
+*metáfora
+'''
+
+
+
